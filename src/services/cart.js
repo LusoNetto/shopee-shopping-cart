95c0f0a88df03e84f8e2481d3ae7a6eb201f1aff
Luso Netto <lucianoaugustodasilvajunior@gmail.com>
2026-02-24 20:41:56 -0300

    feat: create method asign to methods

---
+++ src/services/cart.js
@@ -0,0 +1,17 @@
+let itens = []
+
+async function addItem(item) {
+
+}
+
+async function deleteItem(id) {
+
+}
+
+async function removeItem(id) {
+
+}
+
+async function getTotal() {
+    return itens.reduce()
+}
